bbe5d414a96820a1ad6f7982e537d83537414773
akhilmhdh <akhilmhdh@gmail.com>
2020-03-27 20:01:49 +0530

    =first commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from network import Net
+from PIL import Image
+import torch
+from flask import Flask, jsonify, request
+import requests
+import io
+import transform
+
+app = Flask(__name__)
+
+model_dict = torch.load('model/malayalamOCR.pt',
+                        map_location=lambda storage, loc: storage)
+model = Net()
+model.load_state_dict(model_dict["model"])
+
+
+def transform_image(image_bytes):
+    transformations = transform.Compose([transform.Grayscale(1),
+                                         transform.Resize(32),
+                                         transform.ToTensor(),
+                                         transform.Normalize([0.5], [0.5])
+                                         ])
+    image = Image.open(io.BytesIO(image_bytes))
+    return transformations(image).unsqueeze(0)
+
+
+def get_prediction(image_bytes):
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = model(tensor)
+    _, pred = outputs.max(1)
+    return pred.item()
+
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']
+        img_bytes = file.read()
+        alphabet = get_prediction(image_bytes=img_bytes)
+        return jsonify({'network': "CNN", 'class_label': alphabet})
+
+
+# resp = requests.post("https://cr-mal.herokuapp.com/predict",
+#                      files={"file": open('alphabet_test1.png', 'rb')})
+# print(resp)
---
+++ network.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # conv layer on 1->16->32->64
+        self.conv1 = nn.Conv2d(1, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        # adding a pooling layer
+        self.pool = nn.MaxPool2d(2, 2)
+        # linear layer
+        # output volume (W-F+2P)/s+1
+        self.fc1 = nn.Linear(64*4*4, 500)
+        self.fc2 = nn.Linear(500, 48)
+        # adding dropout layer
+        self.dropout = nn.Dropout(0.25)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        # flatten for FC network
+        x = x.view(-1, 64*4*4)
+        x = self.dropout(x)
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.fc2(x)
+        return x
